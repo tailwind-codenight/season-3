d56814384db0b7e9ca026ac03ccda7e24b6fed55
omidbaharifar <weblax.ir@gmail.com>
2024-02-18 03:36:22 +0330

    fat: new eps added

---
+++ e34/tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        vazirMatn: "Vazirmatn RD",
+      },
+      backgroundImage: ({ theme }) => ({
+        gradient60: "linear-gradient(60deg, var(--tw-gradient-stops))",
+        radialGradient: "radial-gradient(var(--tw-gradient-stops))",
+        newGradient: `linear-gradient(90deg,${theme(
+          "colors.red.950",
+        )},#ffff00,#ff28ff,#00ff28)`,
+      }),
+      spacing: {
+        "20px": "20px",
+        2: "2px",
+      },
+      space: {
+        "15px": "15px",
+      },
+      width: {
+        "45px": "45px",
+      },
+      minWidth: {
+        "700px": "700px",
+      },
+      maxHeight: {
+        '50px': '50px',
+      }
+    },
+  },
+  plugins: [],
+};
